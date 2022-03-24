2f79a9d03fbd14476ad1435eee2ca70428922226
Petrnov321 <98708359+Petrnov321@users.noreply.github.com>
2022-03-24 19:07:06 +0100

    Delete mavenRepo/src directory

--- mavenRepo/src/test/java/testSumString.java
+++
@@ -1,9 +0,0 @@
-public class testSumString {
-
-    public static void main(String[] args) {
-        SumString unite = new SumString();
-
-        System.out.println(unite.uniteStrings("Nazdar", " světe"));
-    }
-
-}
